8bf7939c7c1fb6a8d5c9681ecbc1d764cbcaddd3
Pradip Mondal <pradipbd.professional@gmail.com>
2025-05-28 08:36:19 +0600

    problem 2 solve

---
+++ problem_2.js
@@ -0,0 +1,24 @@
+
+// Q. Write a function that returns the sum of two numbers.
+
+let num1 = 10;
+let num2 = 20;
+let result = num1 + num2;
+console.log(result);
+
+
+let digit1 = 25;
+let digit2 = 30;
+function sum(){
+    return digit1 + digit2;
+}
+console.log(sum());
+// sum()
+
+
+function myFunction (a, b) {
+   return a + b;
+};
+console.log(myFunction(10, 11));
+// myFunction(5, 10);
+
