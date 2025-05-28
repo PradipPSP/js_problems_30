dcc461e1aeab7df8d873a51669d0168463f2a68b
Pradip Mondal <pradipbd.professional@gmail.com>
2025-05-28 16:38:41 +0600

    factorial solvation

---
+++ problem_6.js
@@ -0,0 +1,23 @@
+
+// Q. Write a function to calculate the factorial of a number?
+
+
+// function fact(n) { // এটি একটি ফাংশন যার নাম fact এবং এটি একটি প্যারামিটার n গ্রহণ করে। এখানে n হল সেই সংখ্যা যার ফ্যাক্টরিয়াল গণনা করা হবে।
+//     let res = 1; // এখানে res নামের একটি ভ্যারিয়েবল তৈরি করা হয়েছে এবং এর প্রাথমিক মান রাখা হয়েছে ১। এটি পরে ফ্যাক্টরিয়ালের ফলাফল ধারণ করবে।
+//     for (let i = 1; i <= n; i++) { // এই for লুপটি ১ থেকে শুরু হয়ে n পর্যন্ত চলবে।
+//         res *= i; // প্রতিবার লুপ চালানোর সময় res এর সাথে i গুণ করা হবে। মানে, res = res * i;।
+//     }
+//     return res;
+// }
+// console.log(fact(7));
+
+// Fresh Code
+
+function fact(n){
+    let result = 1;
+        for(let i = 1; i <= n; i++){
+            result *= i; 
+        }
+    return result
+};
+console.log(fact(7));
---
+++ prpblem_5.js
@@ -0,0 +1,11 @@
+
+// Q. Write a program that checks if a number is even or odd.
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+
+let numbers = 8;
+if(numbers % 2 === 0){
+   console.log('even')
+} else{
+   console.log('odd')
+};
