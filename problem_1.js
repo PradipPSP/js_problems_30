76add3fbb952aed667fb0e6c2fdbe80735dc97b4
Pradip Mondal <pradipbd.professional@gmail.com>
2025-05-28 07:38:00 +0600

    first commit

---
+++ problem_1.js
@@ -0,0 +1,19 @@
+
+// Q. Write a program to print numbers from 1 to 10.
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numbers);
+
+for(i=1; i<11; i++){
+   console.log(i);
+}
+
+for(i=1; i<=10; i++){
+    console.log(i)
+}
+
+// for(let a = 1;a<10;a++){
+//     for(let b = 1;b<10;b++){
+//         console.log(a + "*" + b + " = " + a*b);
+//     }
+// }
