dafb9a2c0b02eb2fb794b6866db4e83f3fc498c2
Pradip Mondal <pradipbd.professional@gmail.com>
2025-05-28 15:17:02 +0600

    code update

--- problem_3.js
+++ problem_3.js
@@ -6,7 +6,7 @@
 const numbers = [30, 40, 50];
 const descendingNum = numbers.sort((a, b) => b - a);
 const largestNumber = descendingNum[0];
-// console.log(largestNumber);
+// console.log(largestNumber); // 50
 
 
 
@@ -15,7 +15,7 @@ const largestNumber = descendingNum[0];
 const numbers2 = [40, 30, 50];
 const ascendingNum = numbers2.sort((a, b) => a - b);
 const largestNumber2 = ascendingNum[ascendingNum.length-1];
-// console.log(largestNumber2);
+// console.log(largestNumber2); // 50
 
 
 // way-3
@@ -24,7 +24,7 @@ let num1 = 50;
 let num2 = 25;
 let num3 = 45;
 // let largest = Math.max(num1, num2, num3);
-// console.log('The largest number is ' + largest);
+// console.log('The largest number is ' + largest); // 50
 
 
 // way-4
@@ -34,12 +34,12 @@ let num5 = 55;
 let num6 = 95;
 
 if(num4 >= num5 && num4 >= num6){
-    largest = num4;
+    largest = num4; // অর্থ হচ্ছে num4 যদি num5 এবং num6 এর বড় হয় তবে সে বড়
 } else if(num5 >= num4 && num5 >= num6){
-    largest = num5;
+    largest = num5; // অর্থ হচ্ছে num5 যদি num4 এবং num6 এর বড় হয় তবে সে বড়
 } else{
-    largest = num6;
+    largest = num6; // অন্যথায় num6 বড়
 };
 
-console.log('The largest number is ' + largest);
+console.log('The largest number is ' + largest); // 95
 
---
+++ problem_4.js
@@ -0,0 +1,6 @@
+
+// Q. Write a function that reverses a string.
+
+const str = ['Pratim', 'Pradip', 'Moon', 'Emran', 'Protap', 'M Sabid'];
+const reverseStr = str.reverse();
+console.log(reverseStr); // ["M Sabid", "Protap", "Emran", "Moon", "Pradip", "Pratim"]
