c9fcaa464199dabbd35ea8280308903509ac0dc5
Pradip Mondal <pradipbd.professional@gmail.com>
2025-05-30 02:13:01 +0600

    code update

--- basic problems/problem_5.js
+++ basic problems/problem_5.js
@@ -3,9 +3,21 @@
 
 // let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
 
-let numbers = 8;
-if(numbers % 2 === 0){
-   console.log('even')
-} else{
-   console.log('odd')
+// let numbers = 8;
+// if(numbers % 2 === 0){
+//    console.log('even')
+// } else{
+//    console.log('odd')
+// };
+
+function checkEvenOdd (num){
+   if (num % 2 ==0){
+      console.log(`${num} is even number`)
+   } else{
+      console.log(`${num} is odd number`)
+   };
 };
+// let result = checkEvenOdd(17);
+// console.log(result);
+
+checkEvenOdd(11);
