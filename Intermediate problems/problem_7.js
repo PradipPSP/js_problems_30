1bf988ccf57141692f34eccd3198d36c1ab35a76
Pradip Mondal <pradipbd.professional@gmail.com>
2025-05-31 17:37:48 +0600

    write a funcaion on basic calcualor

--- Intermediate problems/problem_7.js
+++ Intermediate problems/problem_7.js
@@ -1,4 +1,37 @@
 
+// Q. Write a program to implement a basic calculator using switch case.
+
+function calculator (a, b, operator) {
+    let result;
+
+    switch(operator){
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            if(b !== 0){
+                result = a / b;
+            } else{
+                result = 'Error: Division by zero';
+            }
+            break;
+        default:
+            result = 'Invalid Operator';
+    }
+    return result;
+};
+console.log(calculator(5, 8, '*'));
+
+
+
+
+/*
 let tell = prompt("What would you  like to do (*,/,+,-)");
 let num1 = prompt("Enter first Number");
 let num2 = prompt("Enter second Number");
@@ -21,4 +54,5 @@ function calculate(a,b) {
     }
 }
 
-alert(calculate(num1, num2));
+alert(calculate(num1, num2))
+*/
