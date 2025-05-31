a184c2cf66fffb592567fac823dac48bcf747ecc
Pradip Mondal <pradipbd.professional@gmail.com>
2025-05-31 17:06:10 +0600

    wirte a function on intersection of 2 arr

--- Intermediate problems/problem_6.js
+++ Intermediate problems/problem_6.js
@@ -1,41 +1,18 @@
 
-#include <iostream>
-#include <vector>
-#include <unordered_set>
-using namespace std;
+// Q. Write a function that finds the intersection of two arrays.
 
-vector<int> intersection(vector<int>& a, vector<int>& b) {
-  
-    // Put all elements of a[] in as
-    unordered_set<int> as(a.begin(), a.end());  
-  
-    unordered_set<int> rs;                    
-    vector<int> res;                            
+// অর্থ ২ টি এ্যারের মধ্যে কমন ইলিমেন্ট কি কি আছে অর্থাৎ ২ টি এ্যারের মধ্যে কমন ইলিমেন্ট খুজে বের করা হচ্ছে ইন্টাসেকশন।
 
-    // Traverse through b[]
-    for (int i = 0; i < b.size(); i++) {
-      
-        // If the element is in as and not yet in rs
-        // a) Add it to the result set
-        // b) Add it to the result array
-        if (as.find(b[i]) != as.end() && 
-            rs.find(b[i]) == rs.end()) {
-            rs.insert(b[i]); 
-            res.push_back(b[i]); 
+function intersection (arr1, arr2){
+    let newArr = [];
+        for (let i = 0; i < arr1.length; i++){
+            for (let j = 0; j < arr2.length; j++){
+                if(arr1[i] == arr2[j]){
+                    newArr.push(arr1[i]);
+                }
+            }
         }
-    }
-
-    return res;
-}
-
-int main() {
-    vector<int> a = {1, 2, 3, 2, 1};
-    vector<int> b = {3, 2, 2, 3, 3, 2};
-
-    vector<int> res = intersection(a, b);
-  
-    for (int i = 0; i < res.size(); i++) 
-        cout << res[i] << " ";
-
-    return 0;
-}
+    return newArr;
+};
+let interSec = intersection([2, 4, 6, 8, 1], [1, 3, 4, 5, 8, 9]);
+console.log(interSec); // [ 4, 8, 1 ]
